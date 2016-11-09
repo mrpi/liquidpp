d8d60912ef137db55171b0f7fd88d828f9ad08fb
Ludger Sprenker <ludger.sprenker@d-velop.de>
2016-11-09 21:13:41 +0100

    Workarround bug of MSVC std::get_time with too short inputs

--- src/liquidpp/filters/Date.hpp
+++ src/liquidpp/filters/Date.hpp
@@ -1,67 +1,69 @@
 #pragma once
 
-#include "Filter.hpp"
 #include "../Expression.hpp"
+#include "Filter.hpp"
 
 #include <boost/date_time/posix_time/conversion.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-namespace liquidpp
-{
-namespace filters
-{
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/stream.hpp>
 
-struct Date : public Filter
-{
-   Expression::Token dateFormat;
+#include <time.h>
 
-   static bool parse(std::tm& t, const std::string& str, const char* pattern)
-   {
-      std::istringstream iss(str);
-      iss >> std::get_time(&t, pattern);
-      if (iss.fail())
-         return false;
-      return true;
-   }
+namespace liquidpp {
+namespace filters {
 
-   virtual Value operator()(Context& c, Value&& val) const override final
-   {
-      if (!val.isStringViewRepresentable())
-         return std::move(val);
+struct Date : public Filter {
+  Expression::Token dateFormat;
 
-      std::tm t = {};
+  static bool parse(boost::posix_time::ptime &t, const std::string &str,
+                    const char *pattern) {
+    std::istringstream iss(str);
+    std::locale loc(std::locale::classic(),
+                    new boost::posix_time::time_input_facet(pattern));
+    iss.imbue(loc);
+    iss >> t;
+    if (iss.fail())
+      return false;
+    return true;
+  }
 
-      auto inDateStr = val.toString();
-      if (inDateStr == "now" || inDateStr == "today")
-      {
-         t = boost::posix_time::to_tm(boost::posix_time::second_clock::local_time());
+  virtual Value operator()(Context &c, Value &&val) const override final {
+    if (!val.isStringViewRepresentable())
+      return std::move(val);
+
+    std::tm t = {};
+
+    auto inDateStr = val.toString();
+    if (inDateStr == "now" || inDateStr == "today") {
+      t = boost::posix_time::to_tm(
+          boost::posix_time::second_clock::local_time());
+    } else {
+      bool succeeded = false;
+      for (auto pattern : {"%Y-%m-%d %H:%M:%S", "%B %d, %Y", "%b %d, %Y"}) {
+        boost::posix_time::ptime pt;
+        if ((succeeded = parse(pt, inDateStr, pattern))) {
+          // Fill missing fields
+          t = boost::posix_time::to_tm(pt);
+
+          break;
+        }
       }
-      else
-      {
-         bool succeeded = false;
-         for (auto pattern : {"%Y-%m-%d %H:%M:%S", "%B %d, %Y", "%b %d, %Y"})
-         {
-            if ((succeeded = parse(t, inDateStr, pattern)))
-               break;
-         }
-         if (!succeeded)
-            return toValue("Invalid date format!");
+      if (!succeeded)
+        return toValue("Invalid date format!");
+    }
 
-         // Fill missing fields
-         t = boost::posix_time::to_tm(boost::posix_time::ptime_from_tm(t));
-      }
+    std::ostringstream oss;
+    oss << std::put_time(&t,
+                         Expression::value(c, dateFormat).toString().c_str());
 
-      std::ostringstream oss;
-      oss << std::put_time(&t, Expression::value(c, dateFormat).toString().c_str());
+    return oss.str();
+  }
 
-      return oss.str();
-   }
-
-   virtual void addAttribute(string_view sv) override final
-   {
-      dateFormat = Expression::toToken(sv);
-   }
+  virtual void addAttribute(string_view sv) override final {
+    dateFormat = Expression::toToken(sv);
+  }
 };
-
 }
 }
